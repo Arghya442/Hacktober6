747a939f984831e92efd827a99898bf95ef312b6
PiedPiper <114767831+Arghya442@users.noreply.github.com>
2022-10-01 12:21:18 +0530

    Add files via upload

---
+++ BreadWrong.py
@@ -0,0 +1,23 @@
+import collections
+class graph:
+   def __init__(self,gdict=None):
+      if gdict is None:
+         gdict = {}
+      self.gdict = gdict
+def bfs(graph, startnode):
+# Track the visited and unvisited nodes using queue
+   seen, queue = set([startnode]), collections.deque([startnode])
+   while queue:
+      vertex = queue.popleft()
+      marked(vertex)
+      for node 
+
+# The graph dictionary
+gdict = { 
+   "a" : set(["b","c"]),
+   "b" : set(["a", "d"]),
+   "c" : set(["a", "d"]),
+   "d" : set(["e"]),
+   "e" : set(["a"])
+}
+bfs(gdict, "a")
